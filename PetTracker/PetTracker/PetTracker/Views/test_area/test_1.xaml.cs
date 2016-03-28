1401b02507c22d260eb9c3932b2ad2fe53d73807
NGE2015 <ngeluis@gmail.com>
2016-03-28 15:30:05 +0100

    full commit_init

---
+++ PetTracker/PetTracker/PetTracker/Views/test_area/test_1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PetTracker.Views.test_area
+{
+    public partial class test_1 : ContentPage
+    {
+        public test_1()
+        {
+            InitializeComponent();
+        }
+
+        void cmd_save_Clicked(object sender,EventArgs e)
+        {
+            msgbox("cmd_save");
+        }
+        void cmd_count_Clicked(object sender, EventArgs e)
+        {
+            msgbox("cmd_count");
+        }
+
+        void msgbox(string _text)
+        {
+
+            DisplayAlert("app", _text, "Cancel");
+
+        }
+
+    }
+}
